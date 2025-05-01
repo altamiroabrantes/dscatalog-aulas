d1bb4cb6522e730ac836ae32d33d3e6c46ef4741
Altamiro Abrantes <altamiropina.psti@poupex.com.br>
2025-05-01 07:53:06 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,23 +1,33 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
-	@Transactional (readOnly = true)
-	public List<Category> findALL() {
-		return categoryRepository.findAll();
+
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findALL() {
+
+		return categoryRepository.findAll().stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+//		List<Category> list = categoryRepository.findAll();
+//		List<CategoryDTO> listDTO = new ArrayList<>();
+//		for (Category cat: list) {
+//			listDTO.add(new CategoryDTO(cat));
+//		}
+
+//		return listDTO;
 	}
 
 }
